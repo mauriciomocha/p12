13a41e3ae7d3bfcfb53f57178ed10a1bee832afb
Estudiante <Estudiante@lab0108>
2017-12-11 08:01:35 -0500

    Completo..........

--- Practica12/src/vista/VentanaPrincipal.java
+++ Practica12/src/vista/VentanaPrincipal.java
@@ -26,6 +26,7 @@ public class VentanaPrincipal extends JFrame implements ActionListener {
 		initComponets();
 		ge = new GestionEquipo();
 		gp=new GestionPaciente();
+		//prueba para subir
 	}
 
 	private void initComponets() {
